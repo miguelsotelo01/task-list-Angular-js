6bfc50d36593e20f98d53453e4666f1247561caf
miguelsotelo01 <miky.a.sotelo.97@gmail.com>
2022-03-07 11:34:05 -0300

    segundo commit, componente header

--- task-list-ar-programa/src/app/app.component.ts
+++ task-list-ar-programa/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'task-list-ar-programa';
+  title:string  = 'Task List';
 }
