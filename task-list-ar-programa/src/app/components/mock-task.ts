a7f9fef8ec0b5bbfaa40f2d0601de8e73611e858
miguelsotelo01 <miky.a.sotelo.97@gmail.com>
2022-03-07 23:15:42 -0300

    agregado el mock de las tareas y el interface de las mismas

---
+++ task-list-ar-programa/src/app/components/mock-task.ts
@@ -0,0 +1,27 @@
+import { Task } from "./Task"
+export const TASKS: Task[]=[
+{
+    id: 1,
+    text:"terminar primer modulo de angular",
+    day:'agosto 5 a las 12:00',
+    reminder:true
+},
+{
+    id: 2,
+    text:"hacer las compras para la cena",
+    day:'agosto 5 a las 17:00',
+    reminder:true
+},
+{
+    id: 3,
+    text:"investigar sobre boostrap",
+    day:'agosto 5 a las 19:00',
+    reminder:false
+},
+{
+    id: 4,
+    text:"leer mi libro favorito",
+    day:'agosto 5 a las 21:00',
+    reminder:true
+}
+]
