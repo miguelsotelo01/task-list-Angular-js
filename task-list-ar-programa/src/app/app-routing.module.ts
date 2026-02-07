3516c964d482a1bd5e9a129ccad8bed7608807a1
miguelsotelo01 <miky.a.sotelo.97@gmail.com>
2026-02-07 12:52:07 -0300

    mejora la estructura de rutas y componentes, actualiza el contenido de About y Footer

--- task-list-ar-programa/src/app/app-routing.module.ts
+++ task-list-ar-programa/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { AboutComponent } from './components/about/about.component'; // Importar el componente
 
-const routes: Routes = [];
+const routes: Routes = [
+  // Ruta por defecto (la lista de tareas)
+  { path: '', component: TasksComponent },
+  // Ruta para el About
+  { path: 'about', component: AboutComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
